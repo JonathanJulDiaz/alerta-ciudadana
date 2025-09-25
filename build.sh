4e9c39596b836128f5429f4eb28cfa5a326018cf
Jonathan Julio <84109304+JonathanJulDiaz@users.noreply.github.com>
2025-09-25 15:13:29 -0500

    Create a superuser

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ set -o errexit  # Detiene el script si hay un error
 pip install -r requirements.txt
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@email.com', 'adminpass')" | python manage.py shell
+
+echo "Build complete"
